348a0e374aa48f02bbad6fb3fcaaecb445481919
Nandana Moriel Bonaris M. M. <111756459+revealeance@users.noreply.github.com>
2026-04-19 23:25:54 +0700

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+
+#include "include/point2d.hpp"
+#include "include/triangle.hpp"
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    vector<Triangle> triangles;
+
+    /*--------------------------
+    Buat loop untuk membuat menginputkan data triangle (3 buah X Y Z data) ke dalam
+    variable triangles
+    --------------------------*/
+    // Tambahkan di sini
+
+
+    /*------------------------*/
+
+
+    /*--------------------------
+    Implementasikan kode untuk melakukan pengecekan apakah data pada variable triangles
+    adalah "sama kaki", "sama sisi", "siku-siku", atau "sembarang"
+    --------------------------*/
+    // Tambahkan di sini
+
+    /*------------------------*/
+
+    return 0;
+}
